65fcb5dddb2524989af4f895360888cce0ed23bf
Ask Solem <ask@celeryproject.org>
2018-07-03 12:10:08 -0700

    Now depends on robinhood-kafka-python 1.3.8 for Python 3.7 support

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ class ve_build_ext(build_ext):
             raise BuildFailed()
 
 
-install_requires = ['robinhood-kafka-python==1.3.7']
+install_requires = ['robinhood-kafka-python==1.3.8']
 
 PY_VER = sys.version_info
 
