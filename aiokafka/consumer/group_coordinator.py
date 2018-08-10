3f11b114ca246d8b16726f309f49551a3dd2a1dc
Ask Solem <ask@celeryproject.org>
2018-08-10 16:37:10 -0700

    [Consumer] [GroupCoordinator]: Crash hard (sys.exit) if we find ourselves with a crashed coordination_routine

--- aiokafka/consumer/group_coordinator.py
+++ aiokafka/consumer/group_coordinator.py
@@ -1,6 +1,7 @@
 import asyncio
 import collections
 import logging
+import sys
 
 from kafka.coordinator.assignors.roundrobin import RoundRobinPartitionAssignor
 from kafka.coordinator.protocol import ConsumerProtocol
@@ -601,6 +602,10 @@ class GroupCoordinator(BaseCoordinator):
                 log.error("Rejoining group -- %s", exc)
                 yield from self._client.force_metadata_update()
                 self.request_rejoin()
+            except Exception as exc:
+                log.error('CRASHING HARD: COORDINATION ROUTINE GOT %r',
+                          exc, exc_info=1)
+                sys.exit(126)
 
         # Closing finallization
         if assignment is not None:
