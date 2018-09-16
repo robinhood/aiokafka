05596f3464809369ada273ec11f2b7e3fdce0e20
Taras <voyn1991@gmail.com>
2018-09-16 21:43:11 +0300

    Add indempotent producer basic implementation

--- aiokafka/util.py
+++ aiokafka/util.py
@@ -27,3 +27,6 @@ PY_341 = sys.version_info >= (3, 4, 1)
 PY_35 = sys.version_info >= (3, 5)
 PY_352 = sys.version_info >= (3, 5, 2)
 NO_EXTENSIONS = bool(os.environ.get('AIOKAFKA_NO_EXTENSIONS'))
+
+INTEGER_MAX_VALUE = 2 ** 31 - 1
+INTEGER_MIN_VALUE = - 2 ** 31
